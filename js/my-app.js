a03a56c35b0e8b04d3331b3e4de3544fab59beee
Olayinka Sagoe <you@example.com>
2018-11-02 15:11:52 +0100

    update

--- js/my-app.js
+++ js/my-app.js
@@ -282,6 +282,14 @@ $(document).on('click', 'a.product-link', function(){
 	
 });
 
+$(document).on('click', 'a.exp-product-link', function(){
+
+    //product_code = "";
+    product_code = $(this).attr('data-product_code');
+    mainView.router.loadPage('experience-product.html');
+
+});
+
 
 	myApp.onPageInit('single-product', function (page) {
 	//alert(product_code);
@@ -848,10 +856,10 @@ myApp.onPageInit('exp-cat-list', function (page) {
 		 prd_itm += '<div class="single-shop-list">';
 		 prd_itm += '<div class="shop-inner">';
 		 prd_itm += '<div class="shop-img">';
-		 prd_itm += '<a href="experience-product.html" class="product-link" data-product_code="'+value.product_code+'"><img src="'+value.image+'" alt=""/></a>';
+		 prd_itm += '<a href="experience-product.html" class="exp-product-link" data-product_code="'+value.product_code+'"><img src="'+value.image+'" alt=""/></a>';
 		 prd_itm += '</div>';
 		 prd_itm += '<div class="shop-content">';
-		 prd_itm += '<h3><a href="experience-product.html" class="product-link" data-product_code="'+value.product_code+'">'+value.product+'</a></h3>';
+		 prd_itm += '<h3><a href="experience-product.html" class="exp-product-link" data-product_code="'+value.product_code+'">'+value.product+'</a></h3>';
 //		 prd_itm += '<div class="pro-rating-s">';
 //		 prd_itm += '<a href="#"><i class="fa fa-star"></i></a>';
 //		 prd_itm += '<a href="#"><i class="fa fa-star"></i></a>';
@@ -1012,10 +1020,7 @@ myApp.onPageInit('experience-product', function (page) {
 
                     resultfaqs += '</div>';
                 }
-
-
-
-
+                result += resultfaqs;
 
 
 
